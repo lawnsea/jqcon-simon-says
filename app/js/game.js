ab691705bdff3ba254eb9c0257ef426b361e864f
Lon Ingram <lawnsea@gmail.com>
2014-02-12 11:25:31 -0800

    Game state machine

--- app/js/game.js
+++ app/js/game.js
@@ -15,7 +15,10 @@ function (
       gameBoardSelector: '.game-board',
       startButtonSelector: '.game-controls .start',
       levelSelector: '.game-controls .level',
-      colors: ['red', 'blue', 'yellow', 'green']
+      colors: ['red', 'blue', 'yellow', 'green'],
+      level: 1,
+      levelDelta: 2,
+      sequenceDelay: 1000
     });
 
     this.after('initialize', function () {
@@ -38,6 +41,78 @@ function (
           return val !== null;
         });
       activations.log('activations');
+
+      function randomSample(count, source) {
+        var result = [];
+
+        for (var i = 0; i < count; i++) {
+          result.push(source[Math.floor(Math.random() * source.length)]);
+        }
+
+        return result;
+      }
+
+      var gameState = startClicks.merge(activations).
+        withStateMachine([], function (state, event) {
+          var value = event.value();
+          var events = [];
+          var expected;
+
+          switch (value) {
+            case 'start':
+              if (state.length < 1) {
+                // start a new sequence
+                state = randomSample(
+                  this.attr.level + this.attr.levelDelta,
+                  this.attr.colors);
+
+                events.push(new Bacon.Next({
+                  what: 'start',
+                  data: state.slice()
+                }));
+              }
+              break;
+            default:
+              if (state.length < 1) {
+                break;
+              }
+
+              expected = state.shift();
+
+              if (expected === value) {
+                if (state.length < 1) {
+                  events.push(new Bacon.Next({
+                    what: 'win'
+                  }));
+                }
+              } else {
+                events.push(new Bacon.Next({
+                  what: 'lose',
+                  data: {
+                    expected: expected,
+                    actual: value
+                  }
+                }));
+                state = [];
+              }
+          }
+
+          return [state, events];
+        }.bind(this));
+
+      gameState.
+        filter(function (e) {
+          return e.what === 'start';
+        }).
+        map('.data').
+        flatMap(Bacon, 'sequentially', this.attr.sequenceDelay).
+        map(function (color) {
+          return {
+            color: color
+          };
+        }).onValue(this, 'trigger', 'activation');
+
+      gameState.log();
     });
   }
 
